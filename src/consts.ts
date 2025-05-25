278b8724686a3cf4fba4c35c3eb673c862961f06
Fitra Rahman <39044004+Rizki36@users.noreply.github.com>
2025-05-25 16:10:03 +0700

    feat: Refactor contact link to use constant for WhatsApp URL across multiple pages

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,4 @@
 
 export const SITE_TITLE = 'Pondok Pesantren Nurul Islam';
 export const SITE_DESCRIPTION = 'Website resmi Pondok Pesantren Nurul Islam, tempat belajar agama dan umum yang terintegrasi di Indonesia.';
+export const CONTACT_US_URL = 'https://api.whatsapp.com/send?phone=6281939000818&text=Assalamualaikum+Mohon+Informasi+PSB+Pondok+Pesantren+Nurul+Islam&source=&data='
